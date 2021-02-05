3209e44d514b1ede233a45f9595cd2b2adac711a
Muhammad-Akhlaq <76590731+Muhammad-Akhlaq@users.noreply.github.com>
2021-02-05 16:20:41 +0500

    different blogwrite model problem solved

--- DAS/fyp/admin.py
+++ DAS/fyp/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from fyp.models import Blog,Contact,Blogwrite
+from fyp.models import Blog,Contact
 # Register your models here.
 admin.site.register(Blog)
 admin.site.register(Contact)
-admin.site.register(Blogwrite)
