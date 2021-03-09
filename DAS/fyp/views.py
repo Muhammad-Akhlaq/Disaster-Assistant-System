7bf156be99e5838e5260b71e0e5959e6dbcbccb1
Muhammad-Akhlaq <76590731+Muhammad-Akhlaq@users.noreply.github.com>
2021-03-09 17:01:28 +0500

    nothing

--- DAS/fyp/views.py
+++ DAS/fyp/views.py
@@ -108,18 +108,12 @@ def events(request,type):
 def weather(request):
     if request.method == 'POST': 
         city = request.POST['city'] 
-        ''' api key might be expired use your own api_key 
-            place api_key in place of appid ="your_api_key_here "  '''
-  
-        # source contain JSON data from API 
-  
+
         source = urllib.request.urlopen( 
             'http://api.openweathermap.org/data/2.5/weather?q='+ city +'&appid=f0b7bbd617bd589d478db5cfb6a08685').read() 
-  
-        # converting JSON data to a dictionary 
+   
         list_of_data = json.loads(source) 
   
-        # data for variable list_of_data 
         data = { 
             "country_code": str(list_of_data['sys']['country']), 
             "coordinate": str(list_of_data['coord']['lon']) + ' '
